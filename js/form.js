85ecc8b7d90a21ab9c00e4b9a2807b488cb72600
Ramon Gamboa <ramongamboa85@gmail.com>
2023-01-25 21:14:43 -0300

    Se agregaron img y se dio funcionalidad al Form

--- js/form.js
+++ js/form.js
@@ -1,9 +1,24 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
 
-    function validateForm() {
-      let x = document.forms["myForm"]["fname"].value;
-      if (x == "") {
-        alert("Name must be filled out");
-        return false;
-      }
-    }
-  
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }else{
+            alert('Datos validados correctamente')
+        }
+
+        form.classList.add('was-validated')
+        form.reset();
+      }, false)
+     
+    })
+})()
